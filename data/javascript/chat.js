72c3b3439f81eccf94f1fcdbcf57587e1c2b01d2
Michal Halabica <m.halabica@gmail.com>
2014-11-25 16:33:26 +0100

    removed autosend

--- data/javascript/chat.js
+++ data/javascript/chat.js
@@ -63,36 +63,3 @@ $(document).ready(function(){
 		}
 	}, 2500);
 });
-
-
-function getCaret(el) { 
-	if (el.selectionStart) return el.selectionStart; 
-	else if (document.selection) { 
-		el.focus(); 
-
-		var r = document.selection.createRange(); 
-		if (r == null) return 0; 
-		var re = el.createTextRange();
-		var rc = re.duplicate(); 
-		re.moveToBookmark(r.getBookmark());
-		rc.setEndPoint('EndToStart', re); 
-		return rc.text.length; 
-	} 
-	return 0; 
-}
-
-function sendOrNewLine(){
-	$("#message").keypress(function(event){
-		var codeKey = parseInt((event.keyCode ? event.keyCode : event.which));
-		if(codeKey == 119){
-			sendMessage();
-		} else if(codeKey == 13) {
-			var content = this.val();
-			var caret = getCaret(this);
-			this.value = content.substring(0, caret) + "\n" + content.substring(carent, content.length - 1);
-			e.stopPropagation();
-		}
-	});
-}
-
-/**/
